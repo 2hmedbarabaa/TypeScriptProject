20c181e9a843140327d9e40830aa07aed4101467
Ahmed Salah Barabaa <ahmedbarabaa3@gmail.com>
2025-01-21 18:55:40 +0300

    the rest of code

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,7 @@ import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/payments.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
-
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
@@ -15,19 +12,35 @@ const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 // list template instance
-const ul= document.querySelector('ul')!;
+const ul = document.querySelector('ul')!;
 const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
   if (type.value === 'invoice') {
-    doc = new invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
   
   list.render(doc, type.value, 'end');
-
 });
+
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
