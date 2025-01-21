b5aa133f3951fc55287dfb964d346503e57c51d5
Ahmed Salah Barabaa <ahmedbarabaa3@gmail.com>
2025-01-21 17:01:21 +0300

    coding

--- public/app.js
+++ public/app.js
@@ -1,33 +1,24 @@
-"use strict";
-//classes
-class invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        return `${this.client} owes ${this.amount} for ${this.details}`;
-    }
-}
-const invOne = new invoice('mario', 'work on mario web', 250);
-const invTwo = new invoice('luigi', 'work on lugi web', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invOne.client = "yishi";
-invOne.amount = 100;
-console.log(invOne, invTwo);
-console.log(invOne, invTwo);
-// const form = document.querySelector('form')!;
+import { invoice } from './classes/invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/payments.js';
 const form = document.querySelector('.new-item-form');
-// console.log(form.children);
-//input
+console.log(form.children);
+// inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,33 @@
-//classes
-class invoice {
-    client: string;
-    details: string;
-    amount:number;
-
-    constructor(c: string, d:string, a:number){
-        this.client= c;
-        this.details= d;
-        this.amount= a;
-    }
-
-    format(){
-        return `${this.client} owes ${this.amount} for ${this.details}`
-    }
-}
-
-const invOne = new invoice('mario', 'work on mario web', 250);
-const invTwo = new invoice('luigi', 'work on lugi web', 300);
-
-let invoices:invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invOne.client = "yishi";
-invOne.amount = 100;
-
-
-console.log(invOne, invTwo );
-
-console.log(invOne,invTwo);
+import { invoice } from './classes/invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/payments.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
 
 
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
 
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-// const form = document.querySelector('form')!;
+// list template instance
+const ul= document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
-const form =document.querySelector('.new-item-form') as HTMLFormElement ;
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
 
-// console.log(form.children);
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  
+  list.render(doc, type.value, 'end');
 
-//input
-const type = document.querySelector('#type') as HTMLSelectElement
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement
-
-const details = document.querySelector('#details') as HTMLInputElement
-const amount = document.querySelector('#amount') as HTMLInputElement
-
-
-form.addEventListener('submit', (e:Event) => {
-    e.preventDefault();
-
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
-    
-})
+});
